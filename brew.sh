e9ac87299aaf63e880a2f2d8a14b1b88281c7ee8
Nik Malhotra <nmal@me.com>
2019-11-18 09:07:08 -0500

    add config

--- brew.sh
+++ brew.sh
@@ -46,14 +46,14 @@ chsh -s /usr/local/bin/bash
 brew install wget --with-iri
 
 # Install RingoJS and Narwhal.
-# Note that the order in which these are installed is important;
-# see http://git.io/brew-narwhal-ringo.
-brew install ringojs
-brew install narwhal
+# # Note that the order in which these are installed is important;
+# # see http://git.io/brew-narwhal-ringo.
+# brew install ringojs
+# brew install narwhal
 
-# Install Python
-brew install python
-brew install python3
+# # Install Python
+# brew install python
+# brew install python3
 
 # Install ruby-build and rbenv
 brew install ruby-build
@@ -75,55 +75,111 @@ brew install sfnt2woff-zopfli
 brew install woff2
 
 # Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
+brew install adns
+brew install gmp
+brew install libgpg-error
+brew install nettle
+brew install python
+brew install asdf
+brew install gnupg
+brew install libksba
+brew install node
+brew install readline
+brew install autoconf
+brew install gnutls
+brew install libmpc
+brew install npth
+brew install sqlite
+brew install automake
+brew install icdiff
+brew install libtasn1
+brew install oniguruma
+brew install svgo
+brew install bash-completion
+brew install icu4c
+brew install libtool
+brew install openssl
+brew install tig
+brew install coreutils
+brew install isl
+brew install libunistring
+brew install p11-kit
+brew install unbound
+brew install direnv
+brew install jq
+brew install libusb
+brew install pcre
+brew install unixodbc
+brew install gdbm
+brew install libassuan
+brew install libxml2
+brew install pcre2
+brew install watchman
+brew install gettext
+brew install libevent
+brew install libxslt
+brew install pinentry
+brew install xz
+brew install git
+brew install libffi
+brew install libyaml
+brew install pkg-config
+brew install yarn
+brew install glib
+brew install libgcrypt
+brew install mpfr
+brew install postgresql
+brew install zlib
+
+# brew install aircrack-ng
+# brew install bfg
+# brew install binutils
+# brew install binwalk
+# brew install cifer
+# brew install dex2jar
+# brew install dns2tcp
+# brew install fcrackzip
+# brew install foremost
+# brew install hashpump
+# brew install hydra
+# brew install john
+# brew install knock
+# brew install netpbm
+# brew install nmap
+# brew install pngcheck
+# brew install socat
+# brew install sqlmap
+# brew install tcpflow
+# brew install tcpreplay
+# brew install tcptrace
 brew install ucspi-tcp # `tcpserver` etc.
 brew install homebrew/x11/xpdf
-brew install xz
+# brew install xz
 
 # Install other useful binaries.
-brew install ack
+# brew install ack
 brew install dark-mode
 #brew install exiv2
 brew install git
 brew install git-lfs
 brew install git-flow
 brew install git-extras
-brew install hub
-brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
+# brew install hub
+# brew install imagemagick --with-webp
+# brew install lua
+# brew install lynx
+# brew install p7zip
+# brew install pigz
+# brew install pv
 brew install rename
-brew install rhino
-brew install speedtest_cli
-brew install ssh-copy-id
-brew install tree
-brew install webkit2png
-brew install zopfli
-brew install pkg-config libffi
-brew install pandoc
+# brew install rhino
+# brew install speedtest_cli
+# brew install ssh-copy-id
+# brew install tree
+# brew install webkit2png
+# brew install zopfli
+# brew install pkg-config libffi
+# brew install pandoc
 
 # Lxml and Libxslt
 brew install libxml2
@@ -136,7 +192,7 @@ brew install heroku/brew/heroku
 heroku update
 
 # Core casks
-brew cask install --appdir="/Applications" alfred
+
 brew cask install --appdir="~/Applications" iterm2
 brew cask install --appdir="~/Applications" java
 brew cask install --appdir="~/Applications" xquartz
@@ -145,17 +201,22 @@ brew cask install --appdir="~/Applications" xquartz
 brew cask install --appdir="/Applications" sublime-text
 brew cask install --appdir="/Applications" atom
 brew cask install --appdir="/Applications" virtualbox
-brew cask install --appdir="/Applications" vagrant
 brew cask install --appdir="/Applications" macdown
 
 # Misc casks
 brew cask install --appdir="/Applications" google-chrome
-brew cask install --appdir="/Applications" firefox
 brew cask install --appdir="/Applications" skype
 brew cask install --appdir="/Applications" slack
 brew cask install --appdir="/Applications" dropbox
 brew cask install --appdir="/Applications" evernote
 brew cask install --appdir="/Applications" 1password
+brew cask install --appdir="/Applications" visual-studio-code
+brew cask install --appdir="/Applications" graphql-playground
+brew cask install --appdir="/Applications" giphy-capture
+brew cask install --appdir="/Applications" keybase
+brew cask install --appdir="/Applications" psequel
+
+
 #brew cask install --appdir="/Applications" gimp
 #brew cask install --appdir="/Applications" inkscape
 
@@ -171,3 +232,4 @@ brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch 
 
 # Remove outdated versions from the cellar.
 brew cleanup
+brew install
